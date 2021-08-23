10d8715746ceb3ec0d535e6452be86e8dc50a078
dcouch440 <dcouch440@gmail.com>
2021-08-23 12:48:58 -0700

    add lint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -40,5 +40,50 @@ module.exports = {
     }],
     'eol-last': ['error', 'always'],
     'no-multiple-empty-lines': 'error',
+    'semi': ['error', 'always'],
+    'react/jsx-sort-props': ['error', {
+      'callbacksLast': true,
+      'shorthandFirst': true,
+      'shorthandLast': false,
+      'ignoreCase': true,
+      'noSortAlphabetically': false,
+      'reservedFirst': false
+    }],
+    'react/jsx-wrap-multilines': ['error', {
+      'declaration': 'parens-new-line',
+      'assignment': 'parens-new-line',
+      'return': 'parens-new-line',
+      'arrow': 'parens-new-line',
+      'condition': 'ignore',
+      'logical': 'ignore',
+      'prop': 'ignore'
+    }],
+    'arrow-parens': ['error', 'as-needed'],
+    'no-trailing-spaces': ['error', { 'skipBlankLines': true, 'ignoreComments': true }],
+    'react/jsx-closing-bracket-location': [
+      1,
+      'line-aligned'
+    ],
+    'react/jsx-max-props-per-line': ['error', {
+      'max-len': 1
+    }],
+    'react/jsx-first-prop-new-line': [1, 'multiline-multiprop'],
+    'react/jsx-tag-spacing': ['error', {
+      'closingSlash': 'never',
+      'beforeSelfClosing': 'always',
+      'afterOpening': 'never',
+      'beforeClosing': 'allow'
+    }],
+    'react/jsx-equals-spacing': [0, 'always'],
+    'semi-spacing': 'error',
+    'indent': ['error', 2],
+    'no-unused-vars': 'warn',
+    'keyword-spacing': 'error',
+    'space-before-function-paren': 'error',
+    'react/react-in-jsx-scope': 'off',
+    'eqeqeq': 'error',
+    'curly': 'error',
+    'block-spacing': ['error', 'always'],
+    'brace-style': ['error', '1tbs'],
   }
-}
+};
