ef8fc8e4e304aa913939401db13df3f51a32246d
dcouch440 <dcouch440@gmail.com>
2021-08-23 10:32:47 -0700

    create component

---
+++ app/components/AnimatedScreenTransition/index.tsx
@@ -0,0 +1,29 @@
+import React, { useEffect, useRef } from 'react'
+import { Animated, StyleProp, ViewStyle } from 'react-native'
+
+interface Props {
+  children: React.ReactNode|React.ReactNode[]
+  style?: StyleProp<ViewStyle>
+}
+
+export default function AnimatedScreenTransition ({
+  children,
+  style
+}: Props) {
+  const translation = useRef<Animated.Value>(new Animated.Value(0)).current
+
+  useEffect(() => {
+    Animated.timing(translation, {
+      toValue: 1,
+      useNativeDriver: true
+    }).start()
+  }, [translation])
+
+  return (
+    <Animated.View
+      style={[{ opacity:  translation }, style]}
+    >
+      {children}
+    </Animated.View>
+  )
+}
