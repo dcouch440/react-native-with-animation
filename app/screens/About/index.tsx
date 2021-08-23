b53b6d566f73cbdd85717b1c39139aa0f7a08376
dcouch440 <dcouch440@gmail.com>
2021-08-23 12:50:38 -0700

    add about page info

--- app/screens/About/index.tsx
+++ app/screens/About/index.tsx
@@ -1,11 +1,25 @@
-import React from 'react'
-import { Text } from 'react-native'
-import AnimatedScreenTransition from '../../components/AnimatedScreenTransition'
+import React from 'react';
+import { StyleSheet, Text } from 'react-native';
+import AnimatedScreenTransition from '../../components/AnimatedScreenTransition';
 
 export default function About () {
   return (
-    <AnimatedScreenTransition>
-      <Text>About</Text>
+    <AnimatedScreenTransition
+      style={styles.container}
+    >
+      <Text style={styles.text}>This is a test application</Text>
+      <Text style={styles.text}>This application tests React Native's Animation API</Text>
     </AnimatedScreenTransition>
-  )
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    textAlign: 'center',
+    fontSize: 30,
+  }
+})
