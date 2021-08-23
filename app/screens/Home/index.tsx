4e1c49f611b5301ea2245919b9426872a2927aba
dcouch440 <dcouch440@gmail.com>
2021-08-23 12:51:10 -0700

    add data entry

--- app/screens/Home/index.tsx
+++ app/screens/Home/index.tsx
@@ -1,14 +1,55 @@
-import React from 'react'
-import { Text } from 'react-native'
-import AnimatedScreenTransition from '../../components/AnimatedScreenTransition'
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+import React from 'react';
+import { Text } from 'react-native';
+import AnimatedScreenTransition from '../../components/AnimatedScreenTransition';
+
+interface IDATA {
+  label: string;
+  value: string;
+  updated: string;
+  country: string;
+  id: number
+
+}
 
 export default function Home () {
 
+  const DATA: IDATA = {
+    label: 'Covid Cases',
+    value: '+3500',
+    country: 'USA',
+    updated: '8/23/2021',
+    id: 1,
+  };
+
+  const DataEntry = ({ label, value, country, updated }: {
+    label: string;
+    value: string;
+    updated: string;
+    country: string;
+  }) => (
+    <>
+      <Text>{label}: {value}</Text>
+      <Text>{country}</Text>
+      <Text>{updated}</Text>
+    </>
+  );
+
   return (
-    <AnimatedScreenTransition>
-      <Text>
-        Home
-      </Text>
+    <AnimatedScreenTransition
+      style={{ alignItems: 'center', justifyContent: 'center', flex: 1 }}
+    >
+      <MaterialCommunityIcons
+        color='black'
+        name='hospital'
+        size={250}
+      />
+      <DataEntry
+        country={DATA.country}
+        label={DATA.label}
+        updated={DATA.updated}
+        value={DATA.value}
+      />
     </AnimatedScreenTransition>
-  )
+  );
 }
