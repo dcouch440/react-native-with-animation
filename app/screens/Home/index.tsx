1e63fbc33e65b5c52c0e67b1b1dbd57dd1c17cde
dcouch440 <dcouch440@gmail.com>
2021-08-23 20:58:02 -0700

    add swipe pagination

--- app/screens/Home/index.tsx
+++ app/screens/Home/index.tsx
@@ -1,9 +1,10 @@
 import { MaterialCommunityIcons } from '@expo/vector-icons';
 import React from 'react';
-import { Text } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
+import Swiper from 'react-native-swiper';
 import AnimatedScreenTransition from '../../components/AnimatedScreenTransition';
 
-interface IDATA {
+interface IData {
   label: string;
   value: string;
   updated: string;
@@ -14,42 +15,60 @@ interface IDATA {
 
 export default function Home () {
 
-  const DATA: IDATA = {
-    label: 'Covid Cases',
-    value: '+3500',
-    country: 'USA',
-    updated: '8/23/2021',
-    id: 1,
-  };
+  const data: IData[] = [
+    {
+      label: 'Hospital One',
+      value: '+3400 Patients A Year',
+      country: 'USA',
+      updated: '8/23/2021',
+      id: 1,
+    },
+    {
+      label: 'Hospital Two',
+      value: '+3500 Patients A Year',
+      country: 'JAPAN',
+      updated: '8/23/2021',
+      id: 2,
+    },
+  ];
 
-  const DataEntry = ({ label, value, country, updated }: {
-    label: string;
-    value: string;
-    updated: string;
-    country: string;
-  }) => (
-    <>
-      <Text>{label}: {value}</Text>
-      <Text>{country}</Text>
-      <Text>{updated}</Text>
-    </>
-  );
-
-  return (
-    <AnimatedScreenTransition
-      style={{ alignItems: 'center', justifyContent: 'center', flex: 1 }}
+  const dataEntry = () => data.map((item: IData, i: number) => (
+    <View
+      key={i}
+      style={styles.slideLayout}
     >
       <MaterialCommunityIcons
         color='black'
         name='hospital'
         size={250}
       />
-      <DataEntry
-        country={DATA.country}
-        label={DATA.label}
-        updated={DATA.updated}
-        value={DATA.value}
-      />
+      <Text>{item.label}: {item.value}</Text>
+      <Text>{item.country}</Text>
+      <Text>{item.updated}</Text>
+    </View>
+  ));
+
+  return (
+    <AnimatedScreenTransition
+      style={styles.container}
+    >
+      <Swiper showsButtons={false}>
+        {dataEntry()}
+      </Swiper>
     </AnimatedScreenTransition>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    flex: 1,
+    backgroundColor: '#a7b4a7'
+  },
+  slideLayout: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
